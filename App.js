9687dbab6bb29ddcf2e393f555a9b46a8be58456
Elyelin Carrasquero <elyelin15@gmail.com>
2021-03-05 18:14:04 -0300

    cambios

--- App.js
+++ App.js
@@ -1,19 +1,91 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+//import { createBottomTabNavigator } from "react-navigation-tabs";
+import {createDrawerNavigator} from 'react-navigation-drawer';
 
-export default function App() {
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open </Text>
+      <Button
+        title="Ir a detalle"
+        onPress={() => navigation.openDrawer()}
+      />
     </View>
   );
-}
+};
+
+HomeScreen.navigationOptions = {
+  drawerIcon: ({tintColor}) => {
+    return <Ionicons name='ios-information-circle' size={20} color={tintColor} />
+  },
+  title: "Home",
+};
+
+const DetalleScreen = ({ navigation }) => {
+  const [cont, setCont] = useState(0);
+  const incrementar = () => setCont(cont + 1);
+
+  useEffect(() => {
+    navigation.setParams({ incrementar });
+  }, [cont]);
+  const lala = navigation.getParam("lala", "Valor por defecto");
+  return (
+    <View style={styles.container}>
+      <Text>Soy el Detalle {cont}</Text>
+      <Button title="Volver" onPress={() => navigation.navigate("MiModal")} />
+    </View>
+  );
+};
+
+DetalleScreen.navigationOptions = ({ navigation }) => {
+  return {
+    title: navigation.getParam("title", "Cargando..."),
+    headerRight: (
+      <Button
+        onPress={navigation.getParam("incrementar")}
+        title="Mas 1"
+        color="#555"
+      />
+    ),
+  };
+};
+
+const AppNavigator = createDrawerNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Detalle: {
+      screen: DetalleScreen,
+    },
+  },
+  {
+    initialRouteName: "Home",  
+    }),
+  
+
+const RootStack = createStackNavigator(
+  {
+    Main: AppNavigator,
+    MiModal: () => <Text>LALA</Text>,
+  },
+  {
+    mode: "modal",
+    headerMode: "none",
+  }
+);
+
+export default createAppContainer(RootStack);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
